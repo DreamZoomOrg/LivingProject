1e2885a4589c140b4b33f00f1452d5f1cea9a5db
jxsWinnie <jxs_winnie@126.com>
2016-08-12 16:40:35 +0800

    require libs

--- LivingClient/js/main.js
+++ LivingClient/js/main.js
@@ -1,3 +1,28 @@
 /**
  * Created by winnie on 16/8/12.
  */
+var thirdpartPath='thirdpart/';
+require.config({
+    shim:{
+        'backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['underscore', 'jquery'],
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    },
+    paths:{
+        'underscore':thirdpartPath+'underscore',
+        'jquery':thirdpartPath+'jquery',
+        'backbone':thirdpartPath+'backbone'
+    }
+});
+require(['underscore','backbone','jquery'],function(){
+    //TODO app start
+    console.log('123');
+});
